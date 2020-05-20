a94cf27cf1653822e5efc377e558b09a87a7da14
Leonti <prishelec@gmail.com>
2020-05-20 17:43:29 +1000

    Use sensor to measure the distance

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
+#include "RunningAverage.h"
+#include <PinButton.h>
 
-uint8_t upPin = A0;
-uint8_t downPin = A1;
+uint8_t upPin = A1;
+uint8_t downPin = A0;
 
 #define R_EN 7
 #define L_EN 8
@@ -18,34 +20,33 @@ uint8_t downPin = A1;
 #define MAX_SPEED 255
 #define SPEED_STEP_DURATION 125
 
+#define MAX_DISTANCE 101
+#define MIN_DISTANCE 75
+
 typedef enum {
     UP_DIR,
     DOWN_DIR,
+    UP_AUTO,
+    DOWN_AUTO,
     OFF
 } MotorState;
 
 unsigned long last_tick = millis();
+unsigned long last_report_tick = millis();
 uint8_t speed = 0;
 
-MotorState state = OFF;
-volatile int position = 0;
+double distance = 0;
 
-void positionChange() {
-  if (state == UP_DIR) {
-    position++;
-  } else if (state == DOWN_DIR) {
-    position--;
-  }
-}
+MotorState state = OFF;
+
+RunningAverage distanceRA(10);
+
+PinButton upButton(upPin);
+PinButton downButton(downPin);
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Hello");
-
-  pinMode(upPin, INPUT);
-  pinMode(upPin, INPUT_PULLUP);
-  pinMode(downPin, INPUT);
-  pinMode(downPin, INPUT_PULLUP);
 
   pinMode(R_EN, OUTPUT);
   digitalWrite(R_EN, LOW);
@@ -56,23 +57,19 @@ void setup() {
   pinMode(L_PWM, OUTPUT);
   digitalWrite(L_PWM, LOW);
 
-//  pinMode(ENCODER_PIN_1, INPUT);
-  pinMode(ENCODER_PIN_2, INPUT);
-
-//  attachInterrupt((uint8_t) digitalPinToInterrupt(ENCODER_PIN_1), positionUp, CHANGE);
-  attachInterrupt((uint8_t) digitalPinToInterrupt(ENCODER_PIN_2), positionChange, CHANGE);
-
   pinMode(TRIG_PIN, OUTPUT); // Sets the trigPin as an Output
   pinMode(ECHO_PIN, INPUT); // Sets the echoPin as an Input
+
+  distanceRA.clear();
 }
 
 void updateSpeed() {
-    if (state == UP_DIR) {
-        analogWrite(L_PWM, 0);
-        analogWrite(R_PWM, speed);
-    } else if (state == DOWN_DIR) {
+    if (state == UP_DIR || state == UP_AUTO) {
         analogWrite(R_PWM, 0);
         analogWrite(L_PWM, speed);
+    } else if (state == DOWN_DIR || state == DOWN_AUTO) {
+        analogWrite(L_PWM, 0);
+        analogWrite(R_PWM, speed);
     }
 }
 
@@ -89,36 +86,89 @@ void start() {
     digitalWrite(L_EN, HIGH);
 }
 
+void updateDistance() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(5);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(20);
+  digitalWrite(TRIG_PIN, LOW);
+  int duration = pulseIn(ECHO_PIN, HIGH);
+  double localDistance = duration*0.034/2;
+  if (localDistance < 150) {
+    distanceRA.addValue(localDistance);
+  }
+
+  distance = distanceRA.getAverage();
+}
+
 void loop() {
-  if (digitalRead(upPin) == LOW) {
+  upButton.update();
+  downButton.update();
+
+  if (upButton.isDoubleClick()) {
+    state = UP_AUTO;
+    Serial.println("Auto up");
+    updateDistance();
+    if (distance < MAX_DISTANCE) {
+      start();
+      speed = MIN_SPEED;
+    }
+  } else if (downButton.isDoubleClick()) {
+    state = DOWN_AUTO;
+    Serial.println("Auto Down");
+    updateDistance();
+    if (distance > MIN_DISTANCE) {
+      start();
+      speed = MIN_SPEED;
+    }
+  } else if (upButton.isLongClick()) {
     if (state != UP_DIR) {
       start();
       state = UP_DIR;
       speed = MIN_SPEED;
     }
-    updateSpeed();
-  } else if (digitalRead(downPin) == LOW) {
+  } else if (downButton.isLongClick()) {
     if (state != DOWN_DIR) {
       start();
       state = DOWN_DIR;
       speed = MIN_SPEED;
     }
-    updateSpeed();
-  } else {
+  } else if ((state == DOWN_DIR || state == UP_DIR) && (downButton.isReleased() || upButton.isReleased())) {
+    Serial.println("Stopping");
     stop();
   }
 
-  unsigned long now = millis(), diff = now - last_tick;
+  unsigned long now = millis(), diff = now - last_tick, report_diff = now - last_report_tick;
 
-//  if (diff >= SPEED_STEP_DURATION) {
-//    Serial.println("POSITION:");
-//    Serial.println(position);
-//  }
+  if (report_diff >= 100 && (state == UP_AUTO || state == DOWN_AUTO)) {
+    updateDistance();
 
-  if (diff >= SPEED_STEP_DURATION && speed < MAX_SPEED && speed >= MIN_SPEED) {
+    if (state == UP_AUTO) {
+      Serial.print("GOIND UP, DISTANCE:");
+      Serial.println(distance);
+      if (distance > MAX_DISTANCE) {
+        stop();
+      }
+
+    } else if (state == DOWN_AUTO) {
+      Serial.print("GOIND DOWN, DISTANCE:");
+      Serial.println(distance);
+      if (distance < MIN_DISTANCE) {
+        stop();
+      }
+    }
+
+    last_report_tick = now;
+  }
+
+  if (diff >= SPEED_STEP_DURATION && speed < MAX_SPEED && speed >= MIN_SPEED && state != OFF) {
     speed = speed + 5 % (MAX_SPEED + 1);
     last_tick = now;
     Serial.println("SPEED:");
     Serial.println(speed);
+  }
+
+  if (state != OFF) {
+    updateSpeed();
   }
 }
